aa4bc7ae8fac1b30be08c1849c7032b7cf0c34fd
T. Kameda <kame@toiee.jp>
2020-06-15 17:47:31 +0900

    v0.5.2
    ブロックエディタ上でのh1タグとサブヘッダーの組み合わせを調整。

--- bp-blocks.php
+++ bp-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      https://toiee.jp
  * Text Domain:     bp-blocks
  * Domain Path:     /languages
- * Version:         0.5.1
+ * Version:         0.5.2
  *
  * @package         Bp_Blocks
  */
